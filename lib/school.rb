3265d3e85f5c2290bdf0647fbc39041dec82ce67
Alicia Santiago <albrown07@mac.com>
2019-02-19 21:33:23 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,13 +2,17 @@
 class School
   
   def initialize(school_name)
-    @school_name = name
+    @school_name = school_name
     @roster = {}
   end 
   
   def roster=(roster)
     @roster = roster
   end 
+  
+  def roster
+    @roster
+  end
   
   def add_student(student, grade)
     @student = student
@@ -17,18 +21,4 @@ class School
     roster[grade] << student
   end
  # binding.pry
-end
-
-class Dog
-  def initialize(breed)
-    @breed = breed
-  end
- 
-  def breed=(breed)
-    @breed = breed
-  end
- 
-  def breed
-    @breed
-  end
 end
