21b9db6c9efda4011d490e33feae2c1eebfe89d2
Alicia Santiago <albrown07@mac.com>
2019-02-19 21:29:42 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+ require 'pry'
+class School
+  #attr_accessor :name, :roster, :grade
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def roster
+    @roster = roster
+  end 
+  
+  def add_student(student, grade)
+    @student = student
+    @grade = grade
+    roster[grade] = []
+    roster[grade] << student
+  end
+ # binding.pry
+end
